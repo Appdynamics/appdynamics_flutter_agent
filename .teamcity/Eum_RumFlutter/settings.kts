c758cccd9bea01f03a585df2c71c41eee6343a00
Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
2021-05-26 05:33:44 -0700

    Merge pull request #3 in EUM/rum-flutter from CDM-7504-pipelib-sonarqube to master
    Squashed commit of the following:
    
    commit 13f9e626c3fe72213a5aed4a31c9c8b065dc51ba
    Merge: 1827d87 082403e
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Wed May 26 15:12:21 2021 +0300
    
        Merge branch 'master' of ssh://bitbucket.corp.appdynamics.com:7999/eum/rum-flutter into CDM-7504-pipelib-sonarqube
    
        # Conflicts:
        #	.teamcity/Eum_RumFlutter/settings.kts
        #	.teamcity/pom.xml
        #	build.gradle
    
    commit 1827d879d25822c219e7b3a4c07b755b364fde61
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Wed May 26 15:10:36 2021 +0300
    
        Fix incorrect capslock TC projectId
    
    commit fcbaa830a319fccae1bf5b0c2b7a11c4cc1ffd8d
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Wed May 26 14:47:26 2021 +0300
    
        Rename artifact
    
    commit 14e1841dd75eac55f93d4e3853dbfc2cebda37e7
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Wed May 26 14:39:50 2021 +0300
    
        Remove local.properties
    
    commit 43ba0cae277b4fed8e35cadf3d9b7a537afd2671
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Wed May 26 14:39:02 2021 +0300
    
        Update .gitignore for local.properties
    
    commit dfe2fd8ca36bbd6b69832b1cdccd1c1b77873a85
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Wed May 26 14:05:19 2021 +0300
    
        Add ./gradlew pipeline output
    
    commit 1b5e55743296781761b25155e6d5049a53c122d8
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Wed May 26 12:00:41 2021 +0300
    
        Add gradle and appd managed pipeline plugins
    
        https://confluence.corp.appdynamics.com/display/BARE/Requirements+for+using+Pipelib

--- .teamcity/Eum_RumFlutter/settings.kts
+++ .teamcity/Eum_RumFlutter/settings.kts
@@ -1,17 +1,17 @@
-package Eum_RumFlutter
+package EUM_RumFlutter
 
 import com.appdynamics.build.teamcity.pipelib.*
 
 AppdBuildSettings.Builder(AppdBuild.PipeLibType.JAVA_LIB)
-        .teamcityParent("Eum")
+        .teamcityParent("EUM")
         .bitbucketProject("eum")
         .repo("rum-flutter")
-        .projectID("Eum_RumFlutter")
+        .projectID("EUM_RumFlutter")
         .testTargets()
         .buildTargets("build")
         .simpleParams()
         .passwordParams()
         .checkboxParams()
         .selectParams()
-        .projectName("Eum_RumFlutter")
+        .projectName("EUM_RumFlutter")
         .build()
