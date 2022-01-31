adb6c99e096ed2e045e02559d0e699e8d1427e2e
Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
2022-01-31 15:57:18 +0000

    Pull request #48: CDM-8012: Readd ADEUM files
    Merge in EUM/rum-flutter from features/CDM-8012-ga-release to master
    
    Squashed commit of the following:
    
    commit cc459eb1eb4920994505c9127fce47a6e08e022f
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Mon Jan 31 17:05:11 2022 +0200
    
        Readd adeum.gradle instrumentation since it's required for ANR detection to work (bug)
    
    commit dfaa6ba2da437ee600d78542f1a31ead83a66291
    Merge: a3e44ed 7916543
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Thu Jan 27 18:00:25 2022 +0200
    
        Merge
    
    commit a3e44ed6281af3679a546dc5930f460b38992028
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Thu Jan 27 17:43:31 2022 +0200
    
        Readd missing steps
    
    commit 73ef98461e081ffffb7aef1b501adc6ea8a83e99
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Thu Jan 27 16:49:51 2022 +0200
    
        Test pushing to nexus – faster
    
    commit e9311d2ea54b0666d162d9eec9ff4b482bf987e9
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Thu Jan 27 16:47:28 2022 +0200
    
        Test publish to nexus
    
    commit 55abc4a7ffb1b214b81549b51e9b59224d7b0cb1
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Thu Jan 27 16:22:12 2022 +0200
    
        Try again
    
    commit 4268f4ad78b5824543a85b6236de8ee9169da234
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Thu Jan 27 16:03:41 2022 +0200
    
        Test
    
    commit db35d05d376325b5d3999d40210fcc1b9a0be198
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Thu Jan 27 15:39:44 2022 +0200
    
        Test again
    
    commit 110ec31cfdf8fa5a416737dd9cbc0c67cbd02987
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Thu Jan 27 15:28:39 2022 +0200
    
        Fix typo
    
    commit b0c7201feef424f7dd54b9644f3093a34f145608
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Thu Jan 27 15:26:10 2022 +0200
    
        Try differently
    
    commit ff5ab3f3a0ecc540f18f1d01da661106ddfd707c
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Thu Jan 27 15:01:22 2022 +0200
    
        Try to fix dartdoc
    
    commit c1d71f56a0e6cc3b651eb0596c0894d08651e501
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Wed Jan 26 21:04:43 2022 +0200
    
        Try differently
    
    commit 3bab1ca717f844b0f4eec5405d48383e0bdb6996
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Wed Jan 26 20:54:42 2022 +0200
    
        Readd download flutter
    
    commit 4aefbc4485ba2a68e257e61b56ea8b7487b7d554
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Wed Jan 26 20:38:19 2022 +0200
    
        Try differently
    
    commit da0dc5e9ef2e38d51e525c787759ba5e5ab3ca37
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Wed Jan 26 20:28:31 2022 +0200
    
        Fix typo
    
    commit 8d6042ef3118297af4047d18ad04e1ebb163cc68
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Wed Jan 26 20:20:56 2022 +0200
    
        Fix typo
    
    commit 9ea61e09def2464fe18fcd16d7ed0227dc4fc8dd
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Wed Jan 26 20:13:02 2022 +0200
    
        Remove comments that might interfere with CI/CD
    
    commit 68c5daf45f91e1d98912a3c1d70c41e8f6f2290c
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Wed Jan 26 19:56:16 2022 +0200
    
        Fix dartdoc generation
    
    commit 5e7b2776e0bbf4d3bdf07efd98a2d4dfce9f84ba
    Merge: 6874b56 2f33e6a
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Wed Jan 26 19:24:49 2022 +0200
    
        Merge branch 'master' of ssh://bitbucket.corp.appdynamics.com:7999/eum/rum-flutter into features/CDM-8012-ga-release
    
        # Conflicts:
        #	scripts/generate-docs.sh
    
    commit 6874b5679fd2a320cd951308b4960a8c0f918528
    Author: Teodor Ciuraru <teodor.ciuraru@appdynamics.com>
    Date:   Wed Jan 26 19:24:06 2022 +0200
    
        Fix typo
    
    ... and 5 more commits

--- example/lib/app_state/app_state.dart
+++ example/lib/app_state/app_state.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/cupertino.dart';
 
 class AppState extends ChangeNotifier {
-  String _appKey = "SH-AAB-AAE-HDK";
+  String _appKey = "SM-AEZ-YRP";
   bool _crashReportingEnabled = true;
   bool _screenshotsEnabled = true;
 
